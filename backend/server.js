a2a793472036e9916400ba80737a9c8b739c166c
Starrk <aa.razoarmenta@ugto.mx>
2025-11-19 01:21:44 -0600

    Arregle un problema con el puerto del backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-//const puerto = process.env.PORT || 3001;
+const puerto = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors());
